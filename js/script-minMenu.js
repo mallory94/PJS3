7b96a1bf74b3651f7dbe788a6c592020d8820a32
root <root@mallory-PC.localdomain>
2019-10-20 16:55:23 +0200

    ajout de media queries et scripts js masquant le menu sur petit écran. ajout d'un bouton permettant de le faire apparaitre. Suppression de la partie 'travaux' présente en double.

---
+++ js/script-minMenu.js
@@ -0,0 +1,63 @@
+var estMin = false;
+var btnMin = document.querySelector('.minMenu')
+
+var elementsACacher = document.querySelector('.menu')
+var header = document.querySelector('.header')
+var banderole = document.querySelector('.banderole')
+
+function minimiseMenu(){
+    elementsACacher.classList.add('menu-cache')
+    estMin = true;
+    banderole.classList.remove('banderole-cachee');
+    renommeBouton();
+    console.log("minimise")
+}
+
+
+function maximiseMenu(){
+    elementsACacher.classList.remove('menu-cache')
+    estMin = false;
+    banderole.classList.add('banderole-cachee');
+    renommeBouton();
+    console.log("maximise")
+}
+
+btnMin.onclick = function minMaxMenu(){
+    if (estMin == true) {
+        maximiseMenu();
+        
+    }
+
+    else {
+        
+        minimiseMenu();
+    }
+}
+
+function renommeBouton(){
+    if (estMin == true)
+        btnMin.innerHTML = "MENU";
+    else {
+        btnMin.innerHTML = "réduire";
+    }
+}
+
+function autoRedim() {
+    
+    if (window.innerWidth <= 700 && estMin == false) {
+        minimiseMenu()
+    }
+    if (window.innerWidth > 700 && estMin == true) {
+        maximiseMenu();
+        banderole.classList.remove('banderole-cachee');
+    }
+}
+window.onresize = function(){
+    autoRedim();
+}
+
+window.onload = function(){
+    autoRedim();
+}
+
+
